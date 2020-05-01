e1d0807fb09a3caf47e4131d99ab043ea8a1276c
Magdalena <miomayska18@codinburgas.bg>
2020-05-01 15:04:34 +0300

    did stuff

--- Github-Project/Github-Project.cpp
+++ Github-Project/Github-Project.cpp
@@ -5,21 +5,70 @@
 #include<string>
 #include<iomanip>
 using namespace std;
-int n, studentsCount, k;
+const int NUM = 6;
+int n;
 struct Student
 {
 	string firstName;
 	string secondName;
 	string lastName;
 	char EGN[11];
-	int BEL;
-	int MAT;
-	int ENG;
-	int CHEMISTRY;
-	int BIOLOGY;
-	int PHYSICS;
+	double marks[NUM];
 	float a;
-};
+}students[100];
+int displayMenu();
+void beginProgram();
+void read_student(Student& students);
+void insertDate(Student* students);
+void print_student(Student& student);
+void output(Student* students);
+double average(double* a);
+void sortStudents(int n, Student a[]);
+void print_student_success(Student& student);
+void output_success(Student* students);
+void output_success_best(Student* students);
+
+int main()
+{
+	cout << setprecision(2) << setiosflags(ios::fixed);
+	cout << "Number of students: ";
+	cin >> n;
+	beginProgram();
+	cout << endl << endl;
+	return 0;
+}
+
+int displayMenu()
+{
+	int ch;
+	cout << "\n\n MENU: \n ";
+	cout << "\n 1.Entering data for students: \n";
+	cout << "\n 2.Output data for the whole class: \n";
+	cout << "\n 3.List of students by success with all the data: \n";
+	cout << "\n 4.List of students by success: \n";
+	cout << "\n 5.List of students by excellent success: \n";
+	cout << "\n 100 for END  \n";
+	cout << "\n Select  ";
+	cin >> ch;
+	return(ch);
+}
+
+void beginProgram()
+{
+	int j;
+	do {
+		j = displayMenu();
+		switch (j)
+		{
+		case 1:insertDate(students); break;
+		case 2:output(students); break;
+		case 3:sortStudents(n, students); output(students); break;
+		case 4:output_success(students); break;
+		case 5:output_success_best(students); break;
+		case 100: cout << "\n END"; break;
+		}
+	} while (j != 100);
+}
 
 void read_student(Student& students)
 {
@@ -31,20 +80,12 @@ void read_student(Student& students)
 	cin >> students.lastName;
 	cout << "EGN: ";
 	cin >> students.EGN;
-	cout << "BEL: ";
-	cin >> students.BEL;
-	cout << "MAT: ";
-	cin >> students.MAT;
-	cout << "ENG: ";
-	cin >> students.ENG;
-	cout << "CHEMISTRY: ";
-	cin >> students.CHEMISTRY;
-	cout << "BIOLOGY: ";
-	cin >> students.BIOLOGY;
-	cout << "PHYSICS: ";
-	cin >> students.PHYSICS;
+	for (int i = 0; i <= NUM - 1; i++)
+	{
+		cout << i + 1 << " -th mark: ";
+		cin >> students.marks[i];
+	}
 }
-
 
 void insertDate(Student* students)  //, int &studentsCount)
 {
@@ -52,23 +93,24 @@ void insertDate(Student* students)  //, int &studentsCount)
 
 	for (i = 0; i < n; i++)
 		read_student(students[i]);
-
-	//studentsCount++;
 }
-
 void print_student(Student& student)
 {
 	cout << setw(10) << student.firstName << " ";
 	cout << setw(15) << student.secondName << " ";
 	cout << setw(15) << student.lastName << " ";
 	cout << setw(14) << student.EGN << " ";
-	cout << setw(5) << student.BEL << " ";
-	cout << setw(6) << student.MAT << " ";
-	cout << setw(6) << student.ENG << " ";
-	cout << setw(10) << student.CHEMISTRY << " ";
-	cout << setw(10) << student.BIOLOGY << " ";
-	cout << setw(8) << student.PHYSICS << endl;
-	//cout<<students[i].a;
+	for (int i = 0; i <= NUM - 1; i++)
+	{
+		cout << setw(6) << student.marks[i];
+	}
+}
+void print_student_success(Student& student)
+{
+	cout << setw(10) << student.firstName << " ";
+	cout << setw(15) << student.secondName << " ";
+	cout << setw(15) << student.lastName << " ";
+
 }
 
 void output(Student* students)
@@ -79,35 +121,77 @@ void output(Student* students)
 	cout << setw(15) << "lastName";
 	cout << setw(12) << "EGN";
 	cout << setw(10) << "BEL";
-	cout << setw(7) << "MAT";
+	cout << setw(5) << "MAT";
 	cout << setw(7) << "ENG";
-	cout << setw(12) << "CHEMISTRY";
-	cout << setw(12) << "BIOLOGY";
-	cout << setw(10) << "PHYSICS" << endl;
+	cout << setw(6) << "CHM";
+	cout << setw(6) << "BIO";
+	cout << setw(5) << "PHY";
+	cout << setw(17) << "Average success" << endl;
 	for (i = 0; i < n; i++)
+	{
 		print_student(students[i]);
+		cout << setw(7) << average(students[i].marks);
+		cout << endl;
+	}
+	cout << endl;
+}
+void output_success(Student* students)
+{
+	int i;
+	cout << setw(12) << "first name";
+	cout << setw(15) << "secondName";
+	cout << setw(15) << "lastName";
+	cout << setw(17) << "Average success" << endl;
+	for (i = 0; i < n; i++)
+	{
+		print_student_success(students[i]);
+		cout << setw(7) << average(students[i].marks);
+		cout << endl;
+	}
+	cout << endl;
+}
+void output_success_best(Student* students)
+{
+	int i;
+	cout << setw(12) << "first name";
+	cout << setw(15) << "secondName";
+	cout << setw(15) << "lastName";
+	cout << setw(17) << "Average success" << endl;
+	for (i = 0; i < n; i++)
+	{
+		if (average(students[i].marks) >= 5.50)
+		{
+			print_student_success(students[i]);
+			cout << setw(7) << average(students[i].marks);
+			cout << endl;
+		}
+	}
+	cout << endl;
+}
+double average(double* a)
+{
+	double s = 0;
+	for (int j = 0; j < NUM; j++)
+		s += a[j];
+	return s / NUM;
 }
 
-int main()
+
+void sortStudents(int n, Student a[])
 {
-	Student students[100];
 
-
-	cout << "Number of students: ";
-	cin >> n;
-	insertDate(students);
-	output(students);
-
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i <= n - 2; i++)
 	{
-		float s;
-		s = 1.0 * (students[i].BEL + students[i].MAT + students[i].ENG + students[i].CHEMISTRY + students[i].BIOLOGY + students[i].PHYSICS) / 6;
-		students[i].a = s;
+		int k = i;
+		double max = average(a[i].marks);
+		for (int j = i + 1; j <= n - 1; j++)
+			if (average(a[j].marks) > max)
+			{
+				max = average(a[j].marks);
+				k = j;
+			}
+		Student x = a[i]; a[i] = a[k]; a[k] = x;
 	}
-
-
-
-	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
